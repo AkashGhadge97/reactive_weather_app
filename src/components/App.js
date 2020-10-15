91c3b3dc70d05cc0eab6ac8082348cae5533c951
Akash Ghadge <aaghadge97@gamil.com>
2020-10-16 00:37:49 +0530

    Modified Functionality

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,14 @@ export default class App extends React.Component {
       region: null,
       country: null,
       city: "",
+      isCityChanged : false,
       nearByCities : []
     }
 
     this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
     this.handleButtonClick = this.handleButtonClick.bind(this)
   }
   componentDidMount() {
-    console.log("Inside componnt did mount");
     let cityName = this.state.city
     CallWeatherAPI(cityName?cityName:"Satara")
       .then((data) => {
@@ -54,29 +53,54 @@ export default class App extends React.Component {
         })
   }
 
-  handleChange = (event) =>  {
-      this.setState({ city: event.target.value })
+  componentDidUpdate(){
+     if (this.state.isCityChanged){
+       console.log("Update==========================================================");
+      let cityName = this.state.city
+      CallWeatherAPI(cityName?cityName:"Satara")
+        .then((data) => {
+          this.setState({
+            temperature: data.current.temperature,
+            icon: data.current.weather_icons,
+            description: data.current.weather_descriptions,
+            precip: data.current.precip,
+            humidity: data.current.humidity,
+            name: data.location.name,
+            region: data.location.region,
+            country: data.location.country
+          })
+        })
+        .catch((err) => {
+          console.log("Error Occurred while fethcing Wetaher data.");
+        })
+  
+        CallCitiesAPI(cityName?cityName:"Satara")
+          .then((data) => {
+            this.setState({
+              nearByCities: data
+            })
+          })
+          .catch((err) => {
+            console.log("Error Occurred while fethcing newarby cities .");
+          })
+          this.setState({isCityChanged : false})
+     }
   }
 
-  handleSubmit = (event) =>  {
-      this.componentDidMount()
-      event.preventDefault();
+  handleChange = (event) =>  {
+      this.setState({ city: event.target.value , isCityChanged : true })
   }
+
 
   handleButtonClick = (event) => {
-    this.setState({ city : event.target.value})
-    this.componentDidMount()
-    console.log("After component did mount");
+    this.setState({ city : event.target.value ,  isCityChanged : true })
   }
 
   render() {
     return (
       <div className="App">
         <div className="AppHeader">
-          <form onSubmit={this.handleSubmit}>
             <input type="text" value={this.state.city} onChange={this.handleChange} />
-            <input type="submit" value="Search" />
-          </form>
         </div>
         <div className="LeftWindow">
           <div>
